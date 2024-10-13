cfbde9a18e033b29b2812a21f40a8e7d11c9bf42
Zeinab <zeinabansarian6280@gmail.com>
2024-10-13 12:51:41 +0330

    complete aboutUs page

--- js/aboutUs.js
+++ js/aboutUs.js
@@ -1,7 +1,7 @@
 gsap.registerPlugin(ScrollTrigger);
 let aboutNav = document.querySelectorAll(".aboutNav ul li")
 
-
+let navContainer=document.querySelector(".aboutNav")
   
   const lenis = new Lenis()
   
@@ -14,19 +14,18 @@ let aboutNav = document.querySelectorAll(".aboutNav ul li")
   requestAnimationFrame(raf)
 
 
-  let tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".aboutNav",
-      start: "top top",
-      end: "max",
-      pin: true,
-    //   markers: true
-    }
-  });
+  // let tl = gsap.timeline({
+  //   scrollTrigger: {
+  //     trigger: ".aboutNav",
+  //     start: "top top",
+  //     end: "max",
+  //     pin: true,
+  //   //   markers: true
+  //   }
+  // });
 
 
 
-  aboutNav[0].classList.add("activeNav")
   for (let i = 0; i < aboutNav.length; i++) {
       const element = aboutNav[i];
       element.addEventListener("click" , function (params) {
@@ -34,7 +33,7 @@ let aboutNav = document.querySelectorAll(".aboutNav ul li")
               $(".aboutNav ul li").removeClass("activeNav")
               element.classList.add("activeNav")
               lenis.scrollTo(`.scroll-${i+1}` , {
-                  offset : -122,   
+                  offset : 0,   
               })
           
       })
@@ -44,3 +43,147 @@ let aboutNav = document.querySelectorAll(".aboutNav ul li")
 
 
   Scrollbar.initAll(); 
+
+
+
+  const memberSlider = new Swiper('.memberSlider', {
+    // Optional parameters
+  
+    spaceBetween: 40,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.nextMember',
+      prevEl: '.prevMember',
+    },
+    speed:1000,
+    slidesPerView: 4,
+    // And if we need scrollbar
+   
+  });
+  const partnerSlider = new Swiper('.partnerSlider', {
+    // Optional parameters
+  
+    spaceBetween: 40,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.nextpartner',
+      prevEl: '.prevpartner',
+    },
+    speed:1000,
+    slidesPerView: 3,
+    // And if we need scrollbar
+   
+  });
+
+
+  const gallerySlider = new Swiper('.gallerySlider', {
+    slidesPerView:1.5,
+  speed:1000,
+  freeMode:true,
+  grabCursor:true,
+    // If we need pagination
+    pagination: {
+      el: '.galleryPagination',
+      clickable:true,
+    },
+    navigation: {
+        nextEl: '.nextBtn',
+        prevEl: '.prevBtn',
+      },
+    spaceBetween: 40,
+  });
+
+
+  let scrolS = document.querySelectorAll(".scrolS")
+  let darkSection = document.querySelectorAll(".darkSection")
+
+  darkSection.forEach((element,i) => {
+    gsap.to(element, {
+      scrollTrigger: {
+        trigger: element,
+        end: "top -10%",
+        start: "top 90%",
+        
+        scrub: true,
+        // markers: true,
+        onEnter: () => {
+          // pagination.classList.add("darkPagination");
+          // let newH=document.querySelector("header");
+          // newH.classList.remove("dark");
+          // newH.classList.remove("darkLogo");
+          navContainer.classList.remove("darkNav")
+          console.log("element",i);
+          
+        },
+        onLeave: () => {
+          console.log("leave",i);
+          navContainer.classList.add("darkNav")
+          // pagination.classList.remove("darkPagination");
+          // let newH=document.querySelector("header");
+          // newH.classList.add("dark");
+          // newH.classList.add("darkLogo");
+        },
+        onEnterBack: () => {
+          // pagination.classList.add("darkPagination");
+          // let newH=document.querySelector("header");
+          // newH.classList.remove("dark");
+          // newH.classList.remove("darkLogo");
+          navContainer.classList.remove("darkNav")
+        },
+        onLeaveBack: () => {
+          // pagination.classList.remove("darkPagination");
+          // let newH=document.querySelector("header");
+          // newH.classList.add("dark");
+          // newH.classList.add("darkLogo");
+          navContainer.classList.add("darkNav")
+        },
+      },
+    });
+  });
+  scrolS.forEach((element,i) => {
+    gsap.to(element, {
+      scrollTrigger: {
+        trigger: element,
+        end: "top -30%",
+        start: "top 80%",
+        
+        scrub: true,
+        // markers: true,
+        onEnter: () => {
+          // pagination.classList.add("darkPagination");
+          // let newH=document.querySelector("header");
+          // newH.classList.remove("dark");
+          // newH.classList.remove("darkLogo");
+          $(".aboutNav ul li").removeClass("activeNav")
+          aboutNav[i].classList.add("activeNav")
+          console.log("element",i);
+          
+        },
+        // onLeave: () => {
+        //   console.log("leave",i);
+        //   aboutNav[i+1].classList.add("activeNav")
+        //   // pagination.classList.remove("darkPagination");
+        //   // let newH=document.querySelector("header");
+        //   // newH.classList.add("dark");
+        //   // newH.classList.add("darkLogo");
+        // },
+        onEnterBack: () => {
+          // pagination.classList.add("darkPagination");
+          // let newH=document.querySelector("header");
+          $(".aboutNav ul li").removeClass("activeNav")
+          // newH.classList.remove("dark");
+          // newH.classList.remove("darkLogo");
+          aboutNav[i].classList.add("activeNav")
+        },
+        // onLeaveBack: () => {
+        //   // pagination.classList.remove("darkPagination");
+        //   // let newH=document.querySelector("header");
+        //   // newH.classList.add("dark");
+        //   // newH.classList.add("darkLogo");
+        //   aboutNav[i+1].classList.add("activeNav")
+        // },
+      },
+    });
+  });
